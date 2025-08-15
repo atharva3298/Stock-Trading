ba3f151814072a90304399f2177f6a2e331947a7
atharva3298 <atharvuttarwar11@gmail.com>
2025-08-15 12:52:27 +0530

    Add files via upload

---
+++ frontend/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import HomePage from './landing_page/home/HomePage';
+import AboutPage from './landing_page/about/AboutPage';
+
+
+
+import Signup from './landing_page/signup/Signup';
+import ProductsPage from './landing_page/products/ProductsPage';
+import PricingPage from './landing_page/pricing/PricingPage';
+import SupportPage from './landing_page/support/SupportPage';
+import Navbar from './landing_page/Navbar';
+import Footer from './landing_page/Footer';
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <Navbar/>
+  <Routes>
+    <Route path="/"element={<HomePage/>}/>
+    <Route path="/signup"element={<Signup/>}/>
+    <Route path="/about"element={<AboutPage/>}/>
+    <Route path="/product"element={<ProductsPage/>}/>
+    <Route path="/pricing"element={<PricingPage/>}/>
+    <Route path="/support"element={<SupportPage/>}/>
+  </Routes>
+  <Footer/>
+  </BrowserRouter>
+   
+);
+
+
